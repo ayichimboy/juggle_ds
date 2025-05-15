70d7b46c58469f08c8c56baea91bfae22dc51153
ayichimboy <53350925+ayichimboy@users.noreply.github.com>
2025-05-15 12:36:53 -0500

    new updates

---
+++ dev_space/streamlit_app.py
@@ -0,0 +1,11 @@
+import pandas as pd  
+import numpy as np                    
+import matplotlib.pyplot as plt                                                               
+import streamlit as st 
+
+
+# read data/ import data
+# df = pd.read_csv('C:\Users\Owner\juggle_ds\juggle_ds\dev_space\data\iris.csv')
+
+# streamlit setup 
+st.markdown('# Data Dashboard')
---
+++ main/mystreamlit_dashboard.py
@@ -0,0 +1,17 @@
+import numpy as np           
+import pandas as pd                    
+import seaborn as sns          
+import streamlit as st             
+
+
+# design streamlit darshboard
+# state your app title
+# st.header('Dashboard ML')
+st.title("📊 Data Dashboard")
+
+st.header("🚀 Model Performance")
+
+
+st.subheader("🔍 Insights Overview")
+
+
